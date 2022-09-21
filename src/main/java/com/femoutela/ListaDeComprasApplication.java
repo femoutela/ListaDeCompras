7663a82ce3dbd68832be708a150e82004d1d4884
femoutela <femoutela@msn.com>
2022-09-21 11:06:02 -0300

    criando projetos

---
+++ src/main/java/com/femoutela/ListaDeComprasApplication.java
@@ -0,0 +1,13 @@
+package com.femoutela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeComprasApplication.class, args);
+	}
+
+}
